1b9e2846306823d14fa8a8f4c046c06b6fb51861
peppegiu <peppegiu2006@gmail.com>
2025-12-16 21:02:55 -0300

    display project list and todo list in the DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-import To_do from 'src/todo.js'
+import { ProjectArray, Project } from './project-modules/project.js';
+import To_do from './project-modules/todo.js'
+import { CLI_interface } from './project-modules/CLI_Interface.js'
+
+
+const projectArray = new ProjectArray();
+let selectedProject = undefined;
+const sideBar = document.querySelector(".nav-list");
+const projectDisplay = document.querySelector(".project-view");
+
+const projectListDOM = (function () {
+    
+    const displayProjectsList = function () {
+        if (projectArray.isEmpty()) {
+            console.log("No projects created");
+        }
+        else {
+            for (project of projectArray.getProjectArray()[0]) {
+                const projectLine = document.createElement("div");
+                projectLine.id = project.id;
+                projectLine.innerHTML = "<div class='circle-svg'></div>";
+                projectLine.textContent += `   ${project.name}`;
+            }
+        }
+    }
+})
+
+
+const displayProjectTodoList = function (projectID) {
+    // make the project find
+    const project = projectArray.getProjectArray()[0][projectArray.getProjectArray().findIndex((project) => project.id == projectID)];
+    // display the project;
+    for (todo of project.todoList) {
+        const todoElement = document.createElement("div");
+        const checkmark = document.createElement("input");
+        const todoTitle = document.createElement("p");
+        checkmark.id = todo.id;
+        checkmark.setAttribute("type", "checkbox");
+        todoTitle.textContent = todo.title;
+        // todo list must have a checklist 
+        
+    }
+}
+
+const form
---
+++ src/project-modules/project.js
@@ -0,0 +1,56 @@
+export class ProjectArray {
+    #projects = [];
+
+    addProject(project) {
+        this.#projects.push(project);
+    }
+    
+    removeProject(projectID) {
+        const removedProjectIndex = this.#projects.find((project) => project.id == projectID);
+        if (removedProjectIndex !== undefined) {
+            this.#projects.splice(removedProjectIndex, 1);
+            console.log("Project removed.");
+        }
+        else {
+            console.log("Error: project not found.");
+        }
+    }
+
+    isEmpty() {
+        return this.#projects.length == 0;
+    }
+    
+    getProjectArray() {
+        return [this.#projects];
+    }
+}
+
+export class Project {
+    constructor(name) {
+        this.name = name;
+    }
+    id = crypto.randomUUID();
+
+    todoList = [];
+
+    addTodo(todo) {
+        this.todoList.push(todo);
+    }
+
+    removeTodo(todoID) {
+        const removedTodoindex = this.todoList.find((todo) => todo.id == todoID);
+        if (removedTodoindex !== undefined) {
+            this.todoList.splice(removedTodoindex, 1);
+            console.log("Todo removed!");
+        }
+        else {
+            console.log("Error: not found!");
+        }
+        
+    }
+
+    getArrayLength() {
+        return this.todoList.length;
+    }
+    
+}
