dfdd741f38cb39cdd7f4a380ab684bfb2475f3bb
peppegiu <peppegiu2006@gmail.com>
2026-02-01 22:47:33 -0300

    Update package dependencies and improve project structure
    - Upgrade webpack-dev-server to version 5.2.3
    - Change main entry point in package.json to DOMLogic.js
    - Refactor import statement for Todo in applogic.js
    - Enhance template.html with clearer button labels and structure
    - Modify webpack.config.js to use DOMLogic.js as entry point and add source-map support

--- src/project-modules/applogic.js
+++ src/project-modules/applogic.js
@@ -1,5 +1,5 @@
 import { projectArray, Project } from "./project";
-import { Todo } from "./todo";
+import Todo  from "./todo";
 
 export let selectedProject;
 
@@ -10,6 +10,7 @@ export const searchProjectByID = (projectID) => {
       .findIndex((project) => project.id == projectID)
   ];
 };
+
 export const searchTodoByID = (project, todoID) => {
   const foundTodo =
     project.todoList[project.todoList.findIndex((todo) => todo.id == todoID)];
@@ -25,24 +26,14 @@ export const updateSelectedProject = (projectID) => {
   selectedProject = searchProjectByID(projectID);
 };
 
-export const ProjectFormHandler = (() => {
-  const createProject = (formData) => {
-    const project = new Project(formData.get("project-form-name"));
-    return { project };
-  };
-  const getFormData = (formElement) => {
-    const formData = new FormData(formElement);
-    return formData;
-  };
+export const projectFormHandler = (formElement, project) => {
+  const formData = new FormData(formElement);
+  const project = new Project(formData.get("project-form-name"));
 
-  const addProject = (project) => {
-    projectArray.getProjectArray()[0].push(project);
-  };
-  return { createProject, addProject };
-})();
+  projectArray.getProjectArray()[0].push(project);
+};
 
-export const todoFormHandler = () => {
-  const handleData = function (formElement, project) {
+export const todoFormHandler = (formElement, project) => {
     const formData = new FormData(formElement);
     const newTodo = new Todo(
       formData.get("todo-title"),
@@ -53,9 +44,6 @@ export const todoFormHandler = () => {
       
     );
     project.addTodo(newTodo)
-  }
-
-  return {handleData};
 };
 
 export const inspector = () => {
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,17 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/project-modules/DOMLogic.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
